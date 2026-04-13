d32fff230e798fc59b27169e13592352c8ac1449
Dilip Bhanushali <137048001+Dilip9090@users.noreply.github.com>
2026-04-13 21:55:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.py
+++ Difficulty: Easy/Palindrome String/palindrome-string.py
@@ -1,7 +1,16 @@
 class Solution:
     def isPalindrome(self, s):
-        b = s[::-1]
-        if s == b:
-            return True
+        a = 0
+        b = len(s)-1
+        while a < b:
+            if s[a] != s[b] :
+                return False
+            a += 1
+            b -= 1
+        return True    
+        
+        # b = s[::-1]
+        # if s == b:
+        #     return True
         # code here
         
