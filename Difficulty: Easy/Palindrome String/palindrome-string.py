442d9662597fd2132912ec13588445725bb12102
Dilip Bhanushali <137048001+Dilip9090@users.noreply.github.com>
2026-04-13 22:01:16 +0530

    Simplify palindrome check using string slicing
    Refactor palindrome check to use string slicing for simplicity.

--- Difficulty: Easy/Palindrome String/palindrome-string.py
+++ Difficulty: Easy/Palindrome String/palindrome-string.py
@@ -1,16 +1,21 @@
 class Solution:
     def isPalindrome(self, s):
-        a = 0
-        b = len(s)-1
-        while a < b:
-            if s[a] != s[b] :
-                return False
-            a += 1
-            b -= 1
-        return True    
+        #Easy Way  
+        b = s[::-1]
+        if s == b:
+            return True
         
-        # b = s[::-1]
-        # if s == b:
-        #     return True
-        # code here
+        
+        
+        #Complex Way Using Pointer
+        # a = 0
+        # b = len(s)-1
+        # while a < b:
+        #     if s[a] != s[b] :
+        #         return False
+        #     a += 1
+        #     b -= 1
+        # return True
+        
+
         
