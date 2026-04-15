07ec329950c3a33617efc9aba00cc0537f98c420
Dilip Bhanushali <137048001+Dilip9090@users.noreply.github.com>
2026-04-15 12:58:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print GFG n times/print-gfg-n-times.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    def printGfg(self, n):
+        # print(GFG)
+        count = 0
+        while count < n:
+            print("GFG",end=" ")
+            count += 1
+        # Code here
