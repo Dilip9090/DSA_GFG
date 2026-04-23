27841587d6cd37cc741bf5f8249307dc5c7c9940
Dilip Bhanushali <137048001+Dilip9090@users.noreply.github.com>
2026-04-23 20:08:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.py
+++ Difficulty: Easy/Bubble Sort/bubble-sort.py
@@ -1,11 +1,14 @@
 class Solution:
     def bubbleSort(self,arr):
-        #Bubble Sort
+        # code here
         n = len(arr)
+        swap = 0
         
-        for i in range (n):
-            for k in range(0,n-i-1):
+        for i in range(n):
+            for k in range (0,n-i-1):
                 if arr[k] > arr[k+1]:
                     arr[k] , arr[k+1] = arr[k+1], arr[k]
+                    swap += 1
+            if swap == 0:
+                break
         return arr            
-        # code here
