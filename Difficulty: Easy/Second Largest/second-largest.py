d125a119dde0719f448789be48bbbd1b0c8467e1
Dilip Bhanushali <137048001+Dilip9090@users.noreply.github.com>
2026-05-14 19:52:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.py
+++ Difficulty: Easy/Second Largest/second-largest.py
@@ -1,12 +1,27 @@
 class Solution:
     def getSecondLargest(self, arr):
-        # Code Here
-        n = len(arr)
-        arr.sort()
+        largest1 = float('-inf')
+        largest2 = float('-inf')
+
+        for num in arr:
+            if num > largest1:
+                largest2 = largest1
+                largest1 = num
+
+            elif largest1 > num > largest2:
+                largest2 = num
+
+        return -1 if largest2 == float('-inf') else largest2      
         
-        while arr[n-1] != arr[0]:
-            for i in range (n-1,0,-1):
-                if arr[i] > arr[i-1]:
-                    return arr[i-1]
-        while arr[n-1] == arr[0]:
-            return -1
+        
+        
+        # Code Here
+        # n = len(arr)
+        # arr.sort()
+        
+        # while arr[n-1] != arr[0]:
+        #     for i in range (n-1,0,-1):
+        #         if arr[i] > arr[i-1]:
+        #             return arr[i-1]
+        # while arr[n-1] == arr[0]:
+        #     return -1
