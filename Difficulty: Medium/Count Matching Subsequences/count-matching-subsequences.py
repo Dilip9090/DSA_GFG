dcf78c5a805bddabb0548f41178a79886f6843f6
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-06-26 18:26:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Matching Subsequences/count-matching-subsequences.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countWays(self, s1, s2):
+        # code here
+        MOD = 10**9 + 7
+        n, m = len(s1), len(s2)
+
+        dp = [0] * (m + 1)
+        dp[0] = 1
+
+        for i in range(n):
+            for j in range(m - 1, -1, -1):
+                if s1[i] == s2[j]:
+                    dp[j + 1] = (dp[j + 1] + dp[j]) % MOD
+
+        return dp[m]
