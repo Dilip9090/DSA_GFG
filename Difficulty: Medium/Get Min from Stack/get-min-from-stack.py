7f8228477f0647a6efc44424be0243b0f9674a6e
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-06-27 21:16:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Get Min from Stack/get-min-from-stack.py
@@ -0,0 +1,40 @@
+class SpecialStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, x):
+
+        self.stack.append(x)
+
+        if not self.minStack or x <= self.minStack[-1]:
+            self.minStack.append(x)
+
+    def pop(self):
+
+        if not self.stack:
+            return
+
+        if self.stack[-1] == self.minStack[-1]:
+            self.minStack.pop()
+
+        self.stack.pop()
+
+    def peek(self):
+
+        if not self.stack:
+            return -1
+
+        return self.stack[-1]
+
+    def isEmpty(self):
+
+        return len(self.stack) == 0
+
+    def getMin(self):
+
+        if not self.minStack:
+            return -1
+
+        return self.minStack[-1]
