b378c83ec7e3f447b44bfcc51a13508264a5c56e
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-07-14 21:27:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Non-Zero Number/smallest-non-zero-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find(self, arr):
+        # code here
+        need = 0
+        for x in reversed(arr):
+            need = (need + x + 1) // 2
+        return max(1, need)
+        
