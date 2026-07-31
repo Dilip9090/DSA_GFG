c4c7c3c7538ffce980908575c18b27e80a53725d
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-07-31 20:39:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Permutation/next-permutation.py
+++ Difficulty: Medium/Next Permutation/next-permutation.py
@@ -2,23 +2,26 @@ class Solution:
     def nextPermutation(self, arr):
         # code here
         n = len(arr)
-        i = n - 2
-        while i >= 0 and arr[i] >= arr[i + 1]:
-            i -= 1
-            
-        if i == -1:
+        idx = -1
+        
+        for i in range(n-2,-1,-1):
+            if arr[i] < arr[i+1]:
+                idx = i
+                break
+        if idx == -1:
             arr.reverse()
             return arr
-        j = n - 1
-        
-        while arr[j] <= arr[i]:
-            j -= 1
-        arr[i], arr[j] = arr[j], arr[i]
-        left, right = i + 1, n - 1
+        for j in range(n-1,idx,-1):
+            if arr[j] > arr[idx]:
+                arr[j], arr[idx] = arr[idx], arr[j]
+                break
+        left = idx + 1
+        right = n - 1
         
         while left < right:
             arr[left], arr[right] = arr[right], arr[left]
             left += 1
             right -= 1
-
         return arr
+                
+            
