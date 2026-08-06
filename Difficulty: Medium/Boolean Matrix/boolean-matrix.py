6fe934c66820dc08a1631d9557ab3b1918e466b4
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-08-06 12:19:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Boolean Matrix/boolean-matrix.py
+++ Difficulty: Medium/Boolean Matrix/boolean-matrix.py
@@ -1,24 +1,73 @@
 class Solution:
     def booleanMatrix(self, mat):
-        # code here 
-        n = len(mat)
-        m = len(mat[0])
-
-        rows = [False] * n
-        cols = [False] * m
-
-        # Mark rows and columns containing 1
-        for i in range(n):
-            for j in range(m):
+        # code here     
+        col0 = 0
+        m = len(mat)
+        n = len(mat[0])
+        
+        for i in range(m):
+            for j in range(n):
                 if mat[i][j] == 1:
-                    rows[i] = True
-                    cols[j] = True
-
-        # Update matrix
-        for i in range(n):
-            for j in range(m):
-                if rows[i] or cols[j]:
+                    mat[i][0] = 1
+                    if j != 0:
+                        mat[0][j] = 1
+                    else:
+                        col0 = 1
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                if mat[i][0] == 1 or mat[0][j] == 1:
                     mat[i][j] = 1
-
-        return mat
+        
+        if matrix[0][0] == 1:
+            for j in range(n):
+                mat[0][j] = 1
+        
+        if col0 == 1:
+            for i in range(m):
+                mat[i][0] = 1
+        return mat        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # def markrow(self,mat, row):
+    #         for j in range(len(mat[0])):
+    #             if mat[row][j] != 1:
+    #                 mat[row][j] = -1
+            
+    
+    # def markcol(self,mat, col):    
+    #     for i in range(len(mat)):
+    #         if mat[i][col] != 1:
+    #             mat[i][col] = -1
+                    
+                
+    
+    
+    # def booleanMatrix(self, mat):
+    #     # code here 
+        
+    #     for i in range(len(mat)):
+    #         for j in range(len(mat[0])):
+    #             if mat[i][j] == 1:
+    #                 self.markrow(mat, i)
+    #                 self.markcol(mat, j)
+            
+    #     for k in range(len(mat)):
+    #         for l in range(len(mat[0])):
+    #             if mat[k][l] == -1:
+    #                 mat[k][l] = 1
+    
         
