f545c78ca1182ffaf4eeadfdb4f2fe45efc3b3e4
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-08-14 20:50:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.py
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.py
@@ -1,17 +1,32 @@
 class Solution:
-    def subarrayXor(self, arr, k):
+    def subarrayXor(self, arr, m):
         # code here
-        xor_count = {0: 1}
-
-        xr = 0
+        n = len(arr)
+        mpp = {0: 1}
+        xor = 0
         count = 0
-
-        for num in arr:
-            xr ^= num
-
-            count += xor_count.get(xr ^ k, 0)
-
-            xor_count[xr] = xor_count.get(xr, 0) + 1
-
-        return count
+        for i in range(n):
+            xor ^= arr[i]
+            need = xor ^ m
+            
+            if need in mpp:
+                count += mpp[need]
+            if xor in mpp:    
+                mpp[xor] += 1
+            else:
+                mpp[xor] = 1
+        return count        
         
+        
+        
+        
+        
+        # n = len(arr)
+        # sum1 = 0
+        # for i in range(n):
+        #     xor = 0
+        #     for j in range(i,n):
+        #         xor = xor ^ arr[j]
+        #         if xor == m:
+        #             sum1 += 1
+        # return sum1                 
