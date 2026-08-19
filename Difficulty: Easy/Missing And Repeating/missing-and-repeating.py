31de52c4b289ee28932868d7d125fea3a635c80c
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-08-19 19:51:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.py
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.py
@@ -2,21 +2,34 @@ class Solution:
     def findTwoElement(self, arr):
         # code here
         n = len(arr)
-
-        expected_sum = n * (n + 1) // 2
-        actual_sum = sum(arr)
-
-        expected_sq_sum = n * (n + 1) * (2 * n + 1) // 6
-        actual_sq_sum = sum(x * x for x in arr)
-
-        diff = actual_sum - expected_sum         
-        sq_diff = actual_sq_sum - expected_sq_sum
-
-        sum_xy = sq_diff // diff                 
-
-        repeating = (diff + sum_xy) // 2
-        missing = repeating - diff
-
-        return [repeating, missing]
-
-
+        arr1 = [0] * (n + 1)
+        repet = 0
+        miss = 0
+        
+        for num in arr:
+            arr1[num] += 1
+        
+        for i in range(1, n + 1):
+            if arr1[i] == 2:
+                repet = i
+            elif arr1[i] == 0:
+                miss = i
+        
+        return (repet, miss)        
+        
+        # n = len(arr)
+        # repet = -1
+        # miss = -1
+        
+        # for num in range(1, n + 1):
+        #     count = 0
+        #     for i in range(n):
+        #         if arr[i] == num:
+        #             count += 1
+        #     if count == 2:
+        #         repet = num
+        #     elif count == 0:
+        #         miss = num
+        #     if repet != -1 and miss != -1:
+        #         break
+        # return (repet, miss)            
