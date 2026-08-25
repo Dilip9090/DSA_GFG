ebf7e6db4d87e0d50ec249db0df06a174c81288c
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-08-25 22:25:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.py
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.py
@@ -2,21 +2,61 @@ class Solution:
     def findTwoElement(self, arr):
         # code here
         n = len(arr)
-        SN = (n * (n + 1)) // 2
-        S2N = (n * (n + 1)) * (2 * n + 1) // 6
-        s = 0
-        s2 = 0
+        
+        xr = 0
+        
+        for i in range(0,n,1):
+            xr = xr ^ arr[i]
+            xr = xr ^ (i + 1)
+        
+        bitno = 0
+        while (1):
+            if (xr & (1 << bitno) != 0):
+                break
+            bitno += 1
+        
+        zero = 0
+        one = 0
+        
         for i in range(n):
-            s += arr[i]
-            s2 += arr[i] * arr[i]
+            if (arr[i] & (1 << bitno) != 0):
+                one = one ^ arr[i]
+            else:
+                zero = zero ^ arr[i]
         
-        val1 = s - SN
-        val2 = s2 - S2N
-        val2 = val2 // val1
-        x = (val1 + val2) //2
-        y = x - val1
-        return (x, y)
+        for i in range(1,n + 1):
+            if (i & (1 << bitno) != 0):
+                one = one ^ i
+            else:
+                zero = zero ^ i
+        count = 0        
+        for i in range(n):
+            if arr[i] == zero:
+                count += 1  
         
+        if count == 2:
+            return (zero, one)
+    
+        return (one, zero)
+        
+        
+        
+        
+        # n = len(arr)
+        # SN = (n * (n + 1)) // 2
+        # S2N = (n * (n + 1)) * (2 * n + 1) // 6
+        # s = 0
+        # s2 = 0
+        # for i in range(n):
+        #     s += arr[i]
+        #     s2 += arr[i] * arr[i]
+        
+        # val1 = s - SN
+        # val2 = s2 - S2N
+        # val2 = val2 // val1
+        # x = (val1 + val2) //2
+        # y = x - val1
+        # return (x, y)
         
         
         
