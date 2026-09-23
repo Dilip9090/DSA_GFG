7112635d7046c111958ec85443da5519cc82d8c4
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-09-23 11:36:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of Occurrence/number-of-occurrence.py
+++ Difficulty: Easy/Number of Occurrence/number-of-occurrence.py
@@ -1,9 +1,67 @@
 class Solution:
-    def countFreq(self, arr, target):
-        # code here
+    def find (self, arr, key):
+        #code here
+        first = self.lower(arr, key)
+        if first == -1:
+            return (0)
+        second = self.upper(arr, key, first)
+
+        return ((second - first) + 1)
+
+
+    def lower(self, arr, key):
         n = len(arr)
-        count = 0
-        for i in range(n):
-            if arr[i] == target:
-                count += 1
-        return count        
+        low = 0
+        high = n - 1
+        ans = -1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] == key:
+                ans = mid
+                high = mid - 1
+            elif arr[mid] > key:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ans                 
+
+    def upper(self, arr, key, low):
+        high = len(arr) - 1
+        ans = low
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if arr[mid] == key:
+                ans = mid
+                low = mid + 1
+            elif arr[mid] > key:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return ans  
+# class Solution:
+    def countFreq(self, arr, key):
+        # code here
+        return (self.find(arr, key))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n = len(arr)
+        # count = 0
+        # for i in range(n):
+        #     if arr[i] == target:
+        #         count += 1
+        # return count        
